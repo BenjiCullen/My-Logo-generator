48269bf5405a40aba3606bd1ef85c5f03a71f0f4
Benji Cullen <141322492+BenjiCullen@users.noreply.github.com>
2023-11-28 17:57:01 +1030

    commit of entire logo generator

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# My-Logo-generator
-a simple command line logo generator
+# command line svg logo generator
+
+  [![MIT license](https://img.shields.io/badge/License-MIT-blue.svg)](https://lbesson.mit-license.org/)
+
+  ## Table of Contents
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Licenses](#licenses)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  * [Credits](#credits)
+
+  ## Description
+  a simple user based command line logo generator that takes several user in puts then outputs a .svg file of their generated logo.
+
+  ## Installation
+  to install please install node.js
+
+  ## Usage
+  once downloaded, please type npm install into the command line, then node index.js to run the code
+
+  ## Licenses
+    This project is covered under the MIT license. To learn more about what this means, click the license button at the top.
+
+  ## Contributing
+  N/A
+
+  ## Tests
+  N/A
+
+  ## Questions
+  If you have any further questions, please don't hesitate to contact me directly.
+  GitHub: https://github.com/benjicullen  
+  Email: benculln@hotmail.com
+
+  ## Credits
+  Benjamin Cullen
---
+++ index.js
@@ -0,0 +1,79 @@
+// Inquirer (node), file system module (node), and shapes imports
+const inquirer = require("inquirer");
+const fs = require("fs");
+const { Triangle, Square, Circle } = require("./utils/shapes");
+
+// Function writes the SVG file using user answers from inquirer prompts
+function writeToFile(fileName, answers) {
+  let svgString = "";
+  svgString =
+    '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
+  svgString += "<g>";
+  svgString += `${answers.shape}`;
+
+  // Conditional check takes users input from choices array and then adds polygon properties and shape color to SVG string
+  let shapeChoice;
+  if (answers.shape === "Triangle") {
+    shapeChoice = new Triangle();
+    svgString += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeBackgroundColor}"/>`;
+  } else if (answers.shape === "Square") {
+    shapeChoice = new Square();
+    svgString += `<rect x="73" y="40" width="160" height="160" fill="${answers.shapeBackgroundColor}"/>`;
+  } else {
+    shapeChoice = new Circle();
+    svgString += `<circle cx="150" cy="115" r="80" fill="${answers.shapeBackgroundColor}"/>`;
+  }
+
+  // <text> tag alignes the text within the shape, places it into the middle and gives it a font size of '40'
+  svgString += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.textColor}">${answers.text}</text>`;
+  svgString += "</g>";
+  svgString += "</svg>";
+
+  // Using file system module to generate the svg file and then a "Generated logo.svg" message to the console  
+  fs.writeFile(fileName, svgString, (err) => {
+    err ? console.log(err) : console.log("Generated logo.svg");
+  });
+}
+
+// This is the function that uses the inquirer to prompt the user for their infomation which is then stored temperarly.
+function promptUser() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message:
+          "What text would you like you logo to display? (Enter up to three characters)",
+        name: "text",
+      },
+      {
+        type: "input",
+        message:
+          "Choose text color (Enter color keyword OR a hexadecimal number)",
+        name: "textColor",
+      },
+      {
+        type: "list",
+        message: "What shape would you like the logo to render?",
+        choices: ["Triangle", "Square", "Circle"],
+        name: "shape",
+      },
+      {
+        type: "input",
+        message:
+          "Choose shapes color (Enter color keyword OR a hexadecimal number)",
+        name: "shapeBackgroundColor",
+      },
+    ])
+    .then((answers) => {
+      // Error handling for text prompt
+      if (answers.text.length > 3) {
+        console.log("Must enter a value of no more than 3 characters");
+        promptUser();
+      } else {
+        writeToFile("logo.svg", answers);
+      }
+    });
+}
+
+// Calling promptUser function
+promptUser();
